bc5b509bedcc37050eed4b0972ce176f1314df64
Jon Jackson <jon@acuras.co.uk>
2019-12-13 15:24:02 +0000

    Ran full Identity scaffolding Ran command: dotnet aspnet-codegenerator identity -dc dbContext -sqlite

---
+++ CoreWebAppTest/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+using System;
+using CoreWebAppTest.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CoreWebAppTest.Areas.Identity.IdentityHostingStartup))]
+namespace CoreWebAppTest.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<dbContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("dbContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<dbContext>();
+            });
+        }
+    }
+}
